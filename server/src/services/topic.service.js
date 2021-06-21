0b1951cd9f926f3158a0f95088f6e04afe4c032b
kodeaholic <nathan.codeaholic@gmail.com>
2021-06-20 22:58:14 -0400

    api/mobile update

--- server/src/services/topic.service.js
+++ server/src/services/topic.service.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const { Topic, Chapter } = require('../models');
+const { Topic, Chapter, Lesson } = require('../models');
 const ApiError = require('../utils/ApiError');
 const _ = require('lodash')
 
@@ -11,9 +11,12 @@ const queryTopics = async (filter, options) => {
         // query for chapters
         let topicIDs = results.map(item => item._id)
         chapters = await Chapter.find({'topic': { $in: topicIDs }})
+        let chapterIDs = chapters.map(item => item._id)
+        lessons = await Lesson.find({'chapter': { $in: chapterIDs }})
     }
     // transform data
     topics["chapters"] = chapters
+    topics["lessons"] = lessons
     return topics;
 };
 
