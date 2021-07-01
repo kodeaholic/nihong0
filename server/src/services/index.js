f37527be9bd610ef89c36dbe4d1aa9d9e3fe7060
kodeaholic <nathan.codeaholic@gmail.com>
2021-07-01 06:39:06 -0400

    feature/dialog_board basic api & route

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -5,6 +5,7 @@ module.exports.userService = require('./user.service');
 module.exports.cardService = require('./card.service');
 module.exports.boardService = require('./board.service');
 module.exports.listeningBoardService = require('./listeningBoard.service');
+module.exports.dialogBoardService = require('./dialogBoard.service');
 module.exports.topicService = require('./topic.service');
 module.exports.chapterService = require('./chapter.service');
 module.exports.lessonService = require('./lesson.service');
---
+++ server/src/validations/dialogBoard.validation.js
@@ -0,0 +1,70 @@
+const Joi = require('joi');
+const { objectId } = require('./custom.validation');
+
+const TrackValidation = {
+  content: Joi.string().allow(null, ''),
+  contentMeaning: Joi.string().allow(null, ''),
+  contentFurigana: Joi.string().allow(null, ''),
+  start: Joi.number().allow(null, 0),
+  stop: Joi.number().allow(null, 0),
+  role: Joi.number().allow(null, 0),
+}
+
+const createBoard = {
+  body: Joi.object().keys({
+    title: Joi.string().required(),
+    script: Joi.string().allow(null, ''),
+    subtitle: Joi.string().allow(null, ''),
+    audioSrc: Joi.string().allow(null, ''),
+    level: Joi.string().allow(null, ''),
+    free: Joi.number().required(),
+    tracks: Joi.array().items(TrackValidation)
+  }),
+};
+
+const getBoards = {
+  query: Joi.object().keys({
+    title: Joi.string(),
+    level: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer()
+  }),
+};
+
+const getBoard = {
+  params: Joi.object().keys({
+    boardId: Joi.string().custom(objectId),
+  }),
+};
+
+const updateBoard = {
+  params: Joi.object().keys({
+    boardId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+        title: Joi.string().required(),
+        script: Joi.string().allow(null, ''),
+        subtitle: Joi.string().allow(null, ''),
+        audioSrc: Joi.string().allow(null, ''),
+        level: Joi.string().allow(null, ''),
+        free: Joi.number().required(),
+        tracks: Joi.array().items(TrackValidation)
+    })
+    .min(1),
+};
+
+const deleteBoard = {
+  params: Joi.object().keys({
+    boardId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createBoard,
+  getBoards,
+  getBoard,
+  updateBoard,
+  deleteBoard
+};
