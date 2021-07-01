3c032d29b4695199557673ec131194bed56146a0
kodeaholic <nathan.codeaholic@gmail.com>
2021-07-01 06:16:01 -0400

    feature/dialog_board add schema

---
+++ server/src/models/dialogBoard.model.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+const Schema = mongoose.Schema
+const { userGender } = require('../constants/user.constant');
+const Track = mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: false,
+            trim: false,
+        },
+        contentMeaning: {
+            type: String,
+            required: false
+        },
+        contentFurigana: {
+            type: String,
+            required: false
+        },
+        start: {
+            type: Number,
+            required: false
+        },
+        stop: {
+            type: Number,
+            required: false
+        },
+        role: {
+            type: Number,
+            required: false,
+            default: userGender.MALE, // 1 for male: 2 for female
+        }        
+    }, {
+        _id: false,
+    }
+)
+
+const DialogBoardSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: false,
+            // unique: false,
+        },
+        script: {
+            type: String,
+            required: false,
+            trim: false,
+        },
+        subtitle: {
+            type: String,
+            required: false,
+            trim: false,
+        },
+        level: { 
+            type: String,
+            required: false,
+            trim: false,
+        },
+        audioSrc: {
+            type: String,
+            required: false,
+            trim: false,
+        },
+        free: {
+            type: Number,
+            required: false,
+            default: 1, // 1 = free
+        },
+        tracks: {
+            type: [Track],
+            require: false
+        }
+    },
+    {
+        timestamps: true,
+    }        
+);
+// add plugin that converts mongoose to json
+DialogBoardSchema.plugin(toJSON);
+DialogBoardSchema.plugin(paginate);
+/**
+ * Check if a title is taken
+ * @param {string} title - The board's title
+ * @param {ObjectId} [excludeBoardId] - The id of the board to be excluded
+ * @returns {Promise<boolean>}
+ */
+DialogBoardSchema.statics.isTitleTaken = async function (title, excludeBoardId, level) {
+    const board = await this.findOne({ title, _id: { $ne: excludeBoardId }, level: { $eq: level} });
+    return !!board;
+};
+
+DialogBoardSchema.statics.isTitleTakenForCreating = async function (title, level) {
+    const board = await this.findOne({ title, level: { $eq: level} });
+    return !!board;
+};
+
+/**
+ * @typedef Board
+ */
+const DialogBoard = mongoose.model('DialogBoard', DialogBoardSchema);
+
+module.exports = DialogBoard;
