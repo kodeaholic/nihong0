1d0dc575a9edfe280d7db327a3481074f6d09b7c
seta-nathan <nnguyen4@veritone.com>
2021-07-30 20:49:21 +0700

    dictionary: add extracted vocab

--- server/src/scripts/test.js
+++ server/src/scripts/test.js
@@ -1,13 +1,38 @@
+/** Extract text from vocab */
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Joi = require('joi');
+const path = require('path');
 const _ = require('lodash');
+const { Vocab } = require('../models');
 const { htmlEntityDecode } = require('../helpers/htmlentities');
 const { extractTextFromHTMLString, extractFuriganaFromHTMLString } = require('../helpers/dom');
-
+dotenv.config({ path: path.join(__dirname, '../../.env') }); // export vars to process.env
 const main = async () => {
-  const decoded = `<p style="margin-bottom:14px"><span style="font-size:11pt"><span style="line-height:normal"><span style="font-family:Arial,sans-serif"><span style="font-size:14.0pt"><span style="font-family:&quot;MS Gothic&quot;"><span style="color:red"><ruby style="ruby-align:distribute-space">髪型<rp>(</rp><rt style="font-size:7.0pt; layout-grid-mode:line">かみがた</rt><rp>)</rp></ruby></span></span></span></span></span></span></p>`;
-  console.log(decoded);
-  const extractedFurigana = extractFuriganaFromHTMLString(decoded);
-  console.log(extractedFurigana);
+  const envVarsSchema = Joi.object()
+    .keys({
+      MONGODB_URL: Joi.string().required(),
+    })
+    .unknown();
 
+  const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+  if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+  }
+
+  console.log('... Connecting to ', envVars.MONGODB_URL);
+  await mongoose.connect(envVars.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  console.log('Connected');
+  const result = await Vocab.find()
+    .explain()
+    .then((res) => res[0]);
+  console.log(result.executionStats);
   process.exit(0);
 };
 
