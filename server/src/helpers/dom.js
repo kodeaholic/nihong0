76c4d29d4b7858994dd2a1af634b2d8fcadd351a
seta-nathan <nnguyen4@veritone.com>
2021-07-30 08:31:18 +0700

    add function to extract text from html string

---
+++ server/src/helpers/dom.js
@@ -0,0 +1,37 @@
+const _ = require('lodash');
+const parse = require('html-dom-parser');
+function extractTextFromHTMLString(str) {
+  if (_.isEmpty(str)) return str;
+  string = str.replace(/\n/g, '');
+  let dom = parse(string);
+  if (!_.isArray(dom)) dom = [dom]
+  let text = ''
+  for (let i = 0; i < dom.length; i += 1) {
+    text += traverse(dom[i]);
+  }
+  text = text.replace(/\s/g, '');
+  text = text.replace(/\n/g, '');
+  text = text.replace(/\。/g, '');
+  text = text.replace(/\./g, '');
+  return dom;
+}
+
+function traverse(node) {
+  let text = '';
+  if (!_.isEmpty(node)) {
+    text += '';
+  }
+  if (node.type && node.type === 'tag' && !node.name.includes('rt') && !node.name.includes('rp')) {
+    const children = node.children;
+    let n = _.isArray(node.children) ? node.children.length : 0;
+    if (n)
+      for (let i = 0; i < n; i += 1) {
+        text += traverse(node.children[i]);
+      }
+  } else {
+    text += node.data ? node.data : '';
+  }
+
+  return text;
+}
+module.exports = { extractTextFromHTMLString };
---
+++ server/src/helpers/htmlentities.js
@@ -0,0 +1,26 @@
+function htmlEntityEncode(str) {
+  if (!str || !str.length) return ''
+  var buf = []
+
+  for (var i = str.length - 1; i >= 0; i--) {
+    buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''))
+  }
+
+  return buf.join('')
+}
+
+function htmlEntityDecode(str) {
+  if (!str || !str.length) return ''
+  return str.replace(/&#(\d+);/g, function (match, dec) {
+    return String.fromCharCode(dec)
+  })
+}
+
+function getHtmlEntityCodeFromDecodedString(str) {
+  let string = htmlEntityEncode(str)
+  string = string.replace('&#', '')
+  string = string.replace(';', '')
+  return string
+}
+
+module.exports = { htmlEntityDecode, htmlEntityEncode, getHtmlEntityCodeFromDecodedString }
