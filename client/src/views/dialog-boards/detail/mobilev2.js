c57d60ad1a6655510b2a1aa424fc93c2bf4378ac
seta-nathan <nnguyen4@veritone.com>
2021-08-30 11:02:51 -0700

    Dialog-board: add timeout to webview

--- client/src/views/dialog-boards/detail/mobilev2.js
+++ client/src/views/dialog-boards/detail/mobilev2.js
@@ -15,6 +15,8 @@ import './mobilev2.css'
 import './modal.css'
 import { hhmmssToSeconds } from 'src/helpers/time.helper'
 import { maleAvatarBase64Src, femaleAvatarBase64Src } from './image'
+import Loader from 'src/components/Loader'
+import { sleep } from 'src/helpers/common'
 const DIALOG = {
   STANDARD_SPEED_RATE: 1,
   MALE: 1,
@@ -78,6 +80,7 @@ const MobileDialogBoardV2 = (props) => {
   const [script, setScript] = useState('')
   const [subtitle, setSubtitle] = useState('')
   const [displayScript, setDisplayScript] = useState(true)
+  const [loading, setLoading] = useState(true)
 
   //window.player = undefined
   const toggleScript = (e) => {
@@ -103,48 +106,53 @@ const MobileDialogBoardV2 = (props) => {
   )
   useEffect(() => {
     if (boardId) {
-      dialogBoardService.getBoard(boardId).then((res) => {
-        if (res) {
-          if (
-            res.status === 401 ||
-            res.status === 404 ||
-            res.status === 400 ||
-            res.status === 500
-          ) {
-            toast.error(`Bài học không tồn tại`, {
-              position: 'top-right',
-              autoClose: 3000,
-              hideProgressBar: true,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-            })
-            setRedirecTo({ isRedirected: true, redirectedPath: '/dialog-boards' })
-          } else {
-            setData(res)
-            setFree(res.free)
-            setTitle(res.title)
-            setLevel(res.level)
-            setScript(res.script ? htmlEntityDecode(res.script) : '')
-            setSubtitle(res.subtitle ? htmlEntityDecode(res.subtitle) : '')
-            setAudioSrc(res.audioSrc)
-            let initialTracks = res.tracks
-            let clonedTrackes = [...initialTracks]
-            let resultTracks = clonedTrackes.map(function (item) {
-              let newItem = { ...item }
-              newItem.contentFurigana = htmlEntityDecode(item.contentFurigana)
-              if (_.isEmpty(item.start) || item.start >= 0) newItem.start = '00:00:00'
-              if (_.isEmpty(item.stop) || item.stop >= 0) newItem.stop = '00:00:00'
-              newItem.start = hhmmssToSeconds(newItem.start)
-              newItem.stop = hhmmssToSeconds(newItem.stop)
-              return newItem
-            })
+      async function loadData() {
+        await sleep(3000)
+        dialogBoardService.getBoard(boardId).then((res) => {
+          if (res) {
+            if (
+              res.status === 401 ||
+              res.status === 404 ||
+              res.status === 400 ||
+              res.status === 500
+            ) {
+              toast.error(`Bài học không tồn tại`, {
+                position: 'top-right',
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+              })
+              setRedirecTo({ isRedirected: true, redirectedPath: '/dialog-boards' })
+            } else {
+              setData(res)
+              setFree(res.free)
+              setTitle(res.title)
+              setLevel(res.level)
+              setScript(res.script ? htmlEntityDecode(res.script) : '')
+              setSubtitle(res.subtitle ? htmlEntityDecode(res.subtitle) : '')
+              setAudioSrc(res.audioSrc)
+              let initialTracks = res.tracks
+              let clonedTrackes = [...initialTracks]
+              let resultTracks = clonedTrackes.map(function (item) {
+                let newItem = { ...item }
+                newItem.contentFurigana = htmlEntityDecode(item.contentFurigana)
+                if (_.isEmpty(item.start) || item.start >= 0) newItem.start = '00:00:00'
+                if (_.isEmpty(item.stop) || item.stop >= 0) newItem.stop = '00:00:00'
+                newItem.start = hhmmssToSeconds(newItem.start)
+                newItem.stop = hhmmssToSeconds(newItem.stop)
+                return newItem
+              })
 
-            setTracks(resultTracks)
+              setTracks(resultTracks)
+            }
+            setLoading(false)
           }
-        }
-      })
+        })
+      }
+      loadData()
     }
   }, [boardId])
   const src = {
@@ -222,55 +230,58 @@ const MobileDialogBoardV2 = (props) => {
             ></div>
           </CCol>
         </CRow> */}
-        <div className="viewContainer">
-          <div className="listContainer">
-            {tracks.map(function (item, index) {
-              return <ListItem data={item} key={`track-${index}`} />
-            })}
-          </div>
-          <div className="mainAudioPlayerContainer">
-            <Plyr
-              source={src}
-              options={{
-                resetOnEnd: true,
-                displayDuration: true,
-                speed: { selected: 1, options: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] },
-                controls: ['play', 'progress', 'current-time', 'settings'],
-              }}
-            />
-          </div>
-          {/* <div className="buttonToggleContainer"><ScriptButton /></div> */}
-          <div className="container">
-            <details>
-              <summary>
-                <div className="button">Hiện lời dịch</div>
-                <div className="details-modal-overlay"></div>
-              </summary>
-              <div className="details-modal">
-                <div className="details-modal-close">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="14"
-                    height="14"
-                    viewBox="0 0 14 14"
-                    fill="none"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M13.7071 1.70711C14.0976 1.31658 14.0976 0.683417 13.7071 0.292893C13.3166 -0.0976311 12.6834 -0.0976311 12.2929 0.292893L7 5.58579L1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L5.58579 7L0.292893 12.2929C-0.0976311 12.6834 -0.0976311 13.3166 0.292893 13.7071C0.683417 14.0976 1.31658 14.0976 1.70711 13.7071L7 8.41421L12.2929 13.7071C12.6834 14.0976 13.3166 14.0976 13.7071 13.7071C14.0976 13.3166 14.0976 12.6834 13.7071 12.2929L8.41421 7L13.7071 1.70711Z"
-                      fill="black"
-                    />
-                  </svg>
+        <Loader loading={loading} />
+        {!loading && (
+          <div className="viewContainer">
+            <div className="listContainer">
+              {tracks.map(function (item, index) {
+                return <ListItem data={item} key={`track-${index}`} />
+              })}
+            </div>
+            <div className="mainAudioPlayerContainer">
+              <Plyr
+                source={src}
+                options={{
+                  resetOnEnd: true,
+                  displayDuration: true,
+                  speed: { selected: 1, options: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] },
+                  controls: ['play', 'progress', 'current-time', 'settings'],
+                }}
+              />
+            </div>
+            {/* <div className="buttonToggleContainer"><ScriptButton /></div> */}
+            <div className="container">
+              <details>
+                <summary>
+                  <div className="button">Hiện lời dịch</div>
+                  <div className="details-modal-overlay"></div>
+                </summary>
+                <div className="details-modal">
+                  <div className="details-modal-close">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="14"
+                      height="14"
+                      viewBox="0 0 14 14"
+                      fill="none"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M13.7071 1.70711C14.0976 1.31658 14.0976 0.683417 13.7071 0.292893C13.3166 -0.0976311 12.6834 -0.0976311 12.2929 0.292893L7 5.58579L1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L5.58579 7L0.292893 12.2929C-0.0976311 12.6834 -0.0976311 13.3166 0.292893 13.7071C0.683417 14.0976 1.31658 14.0976 1.70711 13.7071L7 8.41421L12.2929 13.7071C12.6834 14.0976 13.3166 14.0976 13.7071 13.7071C14.0976 13.3166 14.0976 12.6834 13.7071 12.2929L8.41421 7L13.7071 1.70711Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
+                  <div className="details-modal-title">
+                    <h1>Lời dịch</h1>
+                  </div>
+                  <div className="details-modal-content">{renderHTML(subtitle)}</div>
                 </div>
-                <div className="details-modal-title">
-                  <h1>Lời dịch</h1>
-                </div>
-                <div className="details-modal-content">{renderHTML(subtitle)}</div>
-              </div>
-            </details>
+              </details>
+            </div>
           </div>
-        </div>
+        )}
       </>
     )
   }
