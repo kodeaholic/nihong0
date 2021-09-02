bcd0c2e62f18c98f7e8a8a03dbabaa817caa4205
seta-nathan <nnguyen4@veritone.com>
2021-09-02 00:38:26 -0700

    fix-ui-bugs: remove somethings

--- client/src/layout/DefaultLayout.js
+++ client/src/layout/DefaultLayout.js
@@ -5,7 +5,7 @@ const DefaultLayout = () => {
   return (
     <div>
       <AppSidebar />
-      <div className="wrapper d-flex flex-column min-vh-100 bg-light" style={{ paddingTop: 10 }}>
+      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
         <AppHeader />
         <div className="body flex-grow-1 px-3">
           <AppContent />
