23dd17da74aff48e766476151dd23d224c5a0d8b
seta-nathan <nnguyen4@veritone.com>
2021-09-29 14:40:59 -0700

    api: add completed items and activities to user

--- server/src/validations/user.validation.js
+++ server/src/validations/user.validation.js
@@ -1,12 +1,27 @@
 const Joi = require('joi');
 const { password, objectId } = require('./custom.validation');
 
+const CompletedItem = {
+  content: Joi.string().allow(null, ''),
+  time: Joi.number().required(),
+  program: Joi.string().allow(null, ''),
+  level: Joi.string().allow(null, ''),
+  itemId: Joi.string().allow(null, ''),
+};
+
+const Activity = {
+  content: Joi.string().allow(null, ''),
+  time: Joi.number().required(),
+};
+
 const createUser = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().custom(password),
     name: Joi.string().required(),
     role: Joi.string().required().valid('user', 'admin'),
+    completedItems: Joi.array().items(CompletedItem),
+    activities: Joi.array().items(Activity),
   }),
 };
 
@@ -35,6 +50,8 @@ const updateUser = {
       email: Joi.string().email(),
       password: Joi.string().custom(password),
       name: Joi.string(),
+      completedItems: Joi.array().items(CompletedItem),
+      activities: Joi.array().items(Activity),
     })
     .min(1),
 };
