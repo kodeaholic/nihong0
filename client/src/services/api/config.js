d9f01bfc343188e212b90db7d1b27009638724cc
seta-nathan <nnguyen4@veritone.com>
2021-10-01 18:33:59 -0700

    add scripts for own server

--- client/src/services/api/config.js
+++ client/src/services/api/config.js
@@ -1,6 +1,6 @@
 import dataConfig from './server.json'
 export const config =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'PRODUCTION'
     ? process.env.HOSTED_PLATFORM && process.env.HOSTED_PLATFORM === 'heroku'
       ? dataConfig['production-heroku']
       : dataConfig['production-own-server']
